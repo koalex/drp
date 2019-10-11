b55fd8508db2569c647880f468514c838fe9bac5
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-10-11 16:16:15 +0700

    Remove image-webpack-loader

--- client/react-front/webpack/webpack.production.js
+++ client/react-front/webpack/webpack.production.js
@@ -170,7 +170,7 @@ module.exports = merge(common, {
 							limit: 4096
 						}
 					},
-					{
+					/*{
 						loader: 'image-webpack-loader',
 						options: {
 							mozjpeg: {
@@ -191,7 +191,7 @@ module.exports = merge(common, {
 								quality: 75
 							}
 						}
-					}
+					}*/
 				]
 			}
 		]
